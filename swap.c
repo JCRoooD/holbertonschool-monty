8df713236c93864c1b91381b5f718c6fc50e62b7
SMCastr <mariesweetycastro@gmail.com>
2023-09-08 16:49:46 -0400

    forget code to file

--- swap.c
+++ swap.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Pointer to the head of the stack
+ * @line_number: Line number being executed from Monty file
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+    if (!stack || !*stack || !(*stack)->next)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    int temp = (*stack)->n;
+    (*stack)->n = (*stack)->next->n;
+    (*stack)->next->n = temp;
+}

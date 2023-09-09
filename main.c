1b161e2b8b662ea97efc9de79ee73987c901ff99
SMCastr <mariesweetycastro@gmail.com>
2023-09-08 20:40:39 -0400

    Betty

--- main.c
+++ main.c
@@ -5,21 +5,21 @@
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
-		// Check if the user provided the correct number of arguments.
+		/*Check if the user provided the correct number of arguments*/
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	char *filename = argv[1]; // Get the filename from the command line.
-	FILE *file = fopen(filename, "r"); // Attempt to open the file for reading.
+	char *filename = argv[1]; /*Get the filename from the command line*/
+	FILE *file = fopen(filename, "r"); /*Attempt to open the file for reading*/
 
 	if (!file) {
-		// Check if the file could be opened.
+		/*Check if the file could be opened*/
 		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
 
-	// Initialize the stack.
+	/*Initialize the stack*/
 	stack_t *stack = NULL;
 	char *line = NULL;
 	size_t len = 0;
@@ -30,20 +30,20 @@ int main(int argc, char **argv) {
 		char *token;
 		instruction_t instruction;
 
-		// Parse the line and extract the opcode and arguments if any.
+		/*Parse the line and extract the opcode and arguments if any*/
 		token = strtok(line, " \t\n");
 		if (token == NULL || token[0] == '#') {
-			// Skip empty lines and comments.
+			/*Skip empty lines and comments*/
 			continue;
 		}
 
 		instruction.opcode = token;
 
-		// Find the corresponding function for the opcode.
+		/*Find the corresponding function for the opcode.*/
 		instruction.f = get_op_func(token);
 
 		if (!instruction.f) {
-			// Handle the case where an invalid instruction is encountered.
+			/*Handle the case where an invalid instruction is encountered*/
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
 			free(line);
 			fclose(file);
@@ -51,40 +51,40 @@ int main(int argc, char **argv) {
 			exit(EXIT_FAILURE);
 		}
 
-		// Execute the instruction.
+		/*Execute the instruction*/
 		execute_instruction(&stack, &instruction, line_number);
 	}
 
-	fclose(file); // Close the file when done.
+	fclose(file); /*Close the file when done*/
 
-	// Free any allocated memory and clean up resources.
+	/*Free any allocated memory and clean up resources*/ 
 	free(line);
 	free_stack(&stack);
 
 	return EXIT_SUCCESS;
 }
 
-// Define other required functions here:
+/*Define other required functions here:*/ 
 
 void execute_instruction(stack_t **stack, instruction_t *instr, unsigned int line_number) {
-	// Implement instruction execution logic here.
+	/*Emplement instruction execution logic here*/
 	if (strcmp(instr->opcode, "push") == 0) {
-		// Handle the "push" instruction.
-		// You need to parse the argument and push it onto the stack.
+		/*Handle the "push" instruction*/
+		/*You need to parse the argument and push it onto the stack*/
 	} else if (strcmp(instr->opcode, "pop") == 0) {
-		// Handle the "pop" instruction.
+		/*Handle the "pop" instruction*/
 	} else if (strcmp(instr->opcode, "pall") == 0) {
-		// Handle the "pall" instruction.
+		/*Handle the "pall" instruction*/
 	}
-	// Add more cases for other instructions.
+	/*Add more cases for other instructions*/
 }
 
 void free_stack(stack_t **stack) {
-	// Implement code to free memory used by the stack.
-	// You should free each element and then the stack itself.
+	/*Implement code to free memory used by the stack*/
+	/*You should free each element and then the stack itself*/ 
 }
 
 instruction_t *get_op_func(char *opcode) {
-	// Implement a function to get the function pointer for an opcode.
-	// You'll need to iterate through an array of instruction_t structs.
+	/*Implement a function to get the function pointer for an opcode*/
+	/*You'll need to iterate through an array of instruction_t structs*/
 }

738057bc78b3a9dd64367f5384bc730b97f7b1ef
JCRoooD <juancarlos-99@live.com>
2023-09-11 15:13:18 -0400

    fiexed error

--- do_instruc.c
+++ do_instruc.c
@@ -8,13 +8,11 @@
  * Description: This function parses and executes Monty ByteCode instructions
  * using an array of instruction_t structures.
  */
+
 void execute_instruction(stack_t **stack, char *line, unsigned int line_number)
 {
-	char *opcode, *argument;
-	unsigned int value;
-
-	opcode = strtok(line, " \t\n");
-	if (opcode == NULL)
+	char *opcode = strtok(line, " \t\n");
+	if (!opcode)
 		return;
 
 	instruction_t instructions[] = {
@@ -26,49 +24,6 @@ void execute_instruction(stack_t **stack, char *line, unsigned int line_number)
 		{"pint", pint},
 		{"nop", nop},
 		{NULL, NULL}};
-
-	for (int i = 0; instructions[i].opcode != NULL; i++)
-	{
-		if (strcmp(opcode, instructions[i].opcode) == 0)
-		{
-			if (strcmp(opcode, "push") == 0)
-			{
-				argument = strtok(NULL, " \t\n");
-				if (argument == NULL)
-				{
-					fprintf(stderr, "L%u: usage push integer\n", line_number);
-					exit(EXIT_FAILURE);
-				}
-				value = atoi(argument);
-				instructions[i].f(stack, value);
-			}
-			else
-			{
-				instructions[i].f(stack, line_number);
-			}
-			return;
-		}
-	}
-	fprintf(stderr, "L%u: unknown instructions %s\n", line_number, opcode);
-	exit(EXIT_FAILURE);
-}
-
-
-void execute_instruction(stack_t **stack, char *line, unsigned int line_number)
-{
-	char *opcode = strtok(line, " \t\n");
-	if (!opcode) return;
-
-	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"pint", pint},
-		{"nop", nop},
-		{NULL, NULL}
-	};
 
 	for (int i = 0; instructions[i].opcode; i++)
 	{
@@ -84,7 +39,10 @@ void execute_instruction(stack_t **stack, char *line, unsigned int line_number)
 				}
 				instructions[i].f(stack, atoi(arg));
 			}
-			else instructions[i].f(stack, line_number);
+			else
+			{
+				instructions[i].f(stack, line_number);
+			}
 			return;
 		}
 	}

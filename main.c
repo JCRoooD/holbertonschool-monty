14082170c720ab26f873030f6adde0c68826522e
JCRoooD <juancarlos-99@live.com>
2023-09-19 11:08:10 -0400

    added free file deleted it in the main

--- main.c
+++ main.c
@@ -40,24 +40,3 @@ int main(int argc, char *argv[])
 
 	exit(EXIT_SUCCESS);
 }
-
-#include "monty.h"
-
-/**
- * free_stack - Free the memory allocated for the stack and its nodes.
- * @stack: Pointer to the stack.
- *
- * Description: This function frees the memory allocated for the stack and
- * its nodes, ensuring there are no memory leaks.
- */
-void free_stack(stack_t *stack)
-{
-	stack_t *current;
-
-	while (stack != NULL)
-	{
-		current = stack;
-		stack = stack->next;
-		free(current);
-	}
-}
